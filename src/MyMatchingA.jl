e2dc403e19e47dedef4ae3e42a411f6a9c5b3c3a
佐々木彬弘 <sasakiakihiro@sasakiakirahiroshi-no-MacBook-Air.local>
2017-06-18 19:10:02 +0900

    muri

--- src/MyMatchingA.jl
+++ src/MyMatchingA.jl
@@ -2,21 +2,21 @@ module MyMatchingA
 export my_deferred_acceptance
 
 function my_deferred_acceptance(boys_prefs, girls_prefs)
-    m = size(boys_prefs, 1)
-    n = size(girls_prefs, 1)
+   @show m = size(boys_prefs, 1)
+   @show n = size(girls_prefs, 1)
     #その男性が婚約している女性(リストが尽きた独身=0,リストが尽きてない独身=-1)
     #boys_partners[1] = 2; 男性1は女性2と婚約している．
-    boys_partners = fill(-1, m)
+   @show boys_partners = fill(-1, m)
     
-    boys_next_to_propose = ones(Int, m)
+   @show boys_next_to_propose = ones(Int, m)
     
-    girls_partners = zeros(Int, n)
+   @show girls_partners = zeros(Int, n)
     
-    girls_rankings = (m+1)*ones(m,n)    
+   @show girls_rankings = (m+1)*ones(m,n)    
     
     for j in 1:n
         for i in 1:length(girls_prefs[j])
-            girls_rankings[girls_prefs[j][i]][j] = i
+     @show       girls_rankings[girls_prefs[j][i]][j] = i
         end
     end
     
