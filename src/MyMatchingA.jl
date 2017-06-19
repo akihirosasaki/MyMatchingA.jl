eb349e3add291d10c98d16fbdac85b8df2fb0f67
佐々木彬弘 <sasakiakihiro@sasakiakirahiroshi-no-MacBook-Air.local>
2017-06-19 11:09:40 +0900

    test

--- src/MyMatchingA.jl
+++ src/MyMatchingA.jl
@@ -16,15 +16,15 @@ function my_deferred_acceptance(boys_prefs, girls_prefs)
     
     for j in 1:n
        for i in 1:length(girls_prefs[j])
-           @show b = length(girls_prefs[j])
-           @show a = girls_prefs[j][i]
-           @show girls_rankings[a,j] = i
+           b = length(girls_prefs[j])
+           a = girls_prefs[j][i]
+           girls_rankings[a,j] = i
        end
     end
     
     
     while true
-        i = get_single(boys_partners)
+        @show i = get_single(boys_partners)
         if i == 0
             break
         end
@@ -35,7 +35,7 @@ function my_deferred_acceptance(boys_prefs, girls_prefs)
             
 
         #男性iが好きな女性jを探す．
-        j = boys_prefs[i][boys_next_to_propose[i]]
+        @show j = boys_prefs[i][boys_next_to_propose[i]]
 
         #女性jが婚約している相手pを探す．
         p = girls_partners[j]
