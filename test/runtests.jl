5c0163aa689020b115872aab273e2d3f9bef9cc8
akihirosasaki <akihirosasaki0730@gmail.com>
2017-06-27 13:53:29 +0900

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,129 +1,263 @@
-# Test file for Deferred Acceptance algorithm routine
+using MyMatching
+using Base.Test
 
-const file_name = "MyMatchingA.jl"
-const function_name = "my_deferred_acceptance"
+const _deferred_acceptance = my_deferred_acceptance
+const test_matrix = false
 
-include(file_name)
-const fn = getfield(Main, Symbol(function_name))
-
-
-if VERSION >= v"0.5-"
-    using Base.Test
-else
-    using BaseTestNext
-    const Test = BaseTestNext
+function mat2vecs{T<:Integer}(prefs::Matrix{T})
+    return [prefs[1:findfirst(prefs[:, j], 0)-1, j] for j in 1:size(prefs, 2)]
 end
 
-@testset "Testing $file_name" begin
-
-    @testset "$function_name: one-to-one" begin
-        # Males' preference orders over females [1, 2, 3] and unmatched 0
-        m_prefs = [1 3 2 3;
-                   2 1 3 1;
-                   3 2 1 2;
-                   0 0 0 0]
-        # Females' preference orders over males [1, 2, 3, 4] and unmatched 0
-        f_prefs = [3 1 3;
-                   1 2 0;
-                   2 3 2;
-                   4 4 1;
-                   0 0 4]
-
-        # Unique stable matching
-        m_matched_expected = [1, 2, 3, 0]
-        f_matched_expected = [1, 2, 3]
-
-        # Male proposal
-        m_matched_computed, f_matched_computed = fn(m_prefs, f_prefs)
-        @test m_matched_computed == m_matched_expected
-        @test f_matched_computed == f_matched_expected
-
-        # Female proposal
-        f_matched_computed, m_matched_computed = fn(f_prefs, m_prefs)
-        @test m_matched_computed == m_matched_expected
-        @test f_matched_computed == f_matched_expected
+function sort_matches!{T<:Integer}(matches::Vector{T}, indptr::Vector{T})
+    for i in 1:length(indptr)-1
+        sort!(view(matches, indptr[i]:indptr[i+1]-1))
     end
+end
 
-    @testset "$function_name: many-to-one with caps 1" begin
-        # Males' preference orders over females [1, 2, 3] and unmatched 0
-        m_prefs = [1 3 2 3;
-                   2 1 3 1;
-                   3 2 1 2;
-                   0 0 0 0]
-        # Females' preference orders over males [1, 2, 3, 4] and unmatched 0
-        f_prefs = [3 1 3;
-                   1 2 0;
-                   2 3 2;
-                   4 4 1;
-                   0 0 4]
+@testset "Testing deferred acceptance" begin
+    matchings_one_to_one = Array{Dict, 1}()
+    matchings_many_to_one = Array{Dict, 1}()
 
-        # Capacities for females
-        caps = [1, 1, 1]
-        indptr_expected = [1, 2, 3, 4]
+    # Test case: one-to-one
+    m, n = 4, 3
+    # Males' preference orders over females [1, 2, 3] and unmatched 0
+    m_prefs = [1 3 2 3;
+               2 1 3 1;
+               3 2 1 2;
+               0 0 0 0]
+    # Females' preference orders over males [1, 2, 3, 4] and unmatched 0
+    f_prefs = [3 1 3;
+               1 2 0;
+               2 3 2;
+               4 4 1;
+               0 0 4]
+    # Unique stable matching
+    m_matches_m_opt = m_matches_f_opt = [1, 2, 3, 0]
+    f_matches_m_opt = f_matches_f_opt = [1, 2, 3]
+    d = Dict(
+        "m_prefs" => m_prefs,
+        "f_prefs" => f_prefs,
+        "m_matches_m_opt" => m_matches_m_opt,
+        "f_matches_m_opt" => f_matches_m_opt,
+        "m_matches_f_opt" => m_matches_f_opt,
+        "f_matches_f_opt" => f_matches_f_opt,
+    )
+    push!(matchings_one_to_one, d)
 
-        # Unique stable matching
-        m_matched_expected = [1, 2, 3, 0]
-        f_matched_expected = [1, 2, 3]
+    # Test case: one-to-one; from Roth and Sotomayor Example 2.9
+    m, n = 5, 4
+    m_prefs = [
+        1, 2, 3, 4, 0,
+        4, 2, 3, 1, 0,
+        4, 3, 1, 2, 0,
+        1, 4, 3, 2, 0,
+        1, 2, 4, 0, 3,
+    ]
+    m_prefs = reshape(m_prefs, n+1, m)
+    f_prefs = [
+        2, 3, 1, 4, 5, 0,
+        3, 1, 2, 4, 5, 0,
+        5, 4, 1, 2, 3, 0,
+        1, 4, 5, 2, 3, 0,
+    ]
+    f_prefs = reshape(f_prefs, m+1, n)
+    m_matches_m_opt, f_matches_m_opt = [1, 2, 3, 4, 0], [1, 2, 3, 4]
+    m_matches_f_opt, f_matches_f_opt = [4, 1, 2, 3, 0], [2, 3, 4, 1]
+    d = Dict(
+        "m_prefs" => m_prefs,
+        "f_prefs" => f_prefs,
+        "m_matches_m_opt" => m_matches_m_opt,
+        "f_matches_m_opt" => f_matches_m_opt,
+        "m_matches_f_opt" => m_matches_f_opt,
+        "f_matches_f_opt" => f_matches_f_opt,
+    )
+    push!(matchings_one_to_one, d)
 
-        # Male proposal
-        m_matched_computed, f_matched_computed, indptr_computed =
-            fn(m_prefs, f_prefs, caps)
-        @test m_matched_computed == m_matched_expected
-        @test f_matched_computed == f_matched_expected
-        @test indptr_computed == indptr_expected
-    end
+    # Test case: one-to-one; from Roth and Sotomayor Example 2.17
+    m = n = 4
+    m_prefs = [
+        1, 2, 3, 4, 0,
+        2, 1, 4, 3, 0,
+        3, 4, 1, 2, 0,
+        4, 3, 2, 1, 0,
+    ]
+    m_prefs = reshape(m_prefs, n+1, m)
+    f_prefs = [
+        4, 3, 2, 1, 0,
+        3, 4, 1, 2, 0,
+        2, 1, 4, 3, 0,
+        1, 2, 3, 4, 0,
+    ]
+    f_prefs = reshape(f_prefs, m+1, n)
+    m_matches_m_opt = f_matches_m_opt = [1, 2, 3, 4]
+    m_matches_f_opt = f_matches_f_opt = [4, 3, 2, 1]
+    d = Dict(
+        "m_prefs" => m_prefs,
+        "f_prefs" => f_prefs,
+        "m_matches_m_opt" => m_matches_m_opt,
+        "f_matches_m_opt" => f_matches_m_opt,
+        "m_matches_f_opt" => m_matches_f_opt,
+        "f_matches_f_opt" => f_matches_f_opt,
+    )
+    push!(matchings_one_to_one, d)
 
-    @testset "$function_name: many-to-one" begin
-        # From http://www.columbia.edu/~js1353/pubs/qst-many-to-one.pdf
-        # Originally from Gusfield and Irving (1989, Section 1.6.5)
+    # Test case: many-to-one
+    # From Gusfield and Irving (1989, Section 1.6.5)
 
-        m = 11  # Number of students
-        n = 5   # Number of colleges
+    m = 11  # Number of students
+    n = 5   # Number of colleges
 
-        # Students' preference orders over colleges 1, ..., 5 and unmatched 0
-        s_prefs = [3, 1, 5, 4, 0, 2,
-                   1, 3, 4, 2, 5, 0,
-                   4, 5, 3, 1, 2, 0,
-                   3, 4, 1, 5, 0, 2,
-                   1, 4, 2, 0, 3, 5,
-                   4, 3, 2, 1, 5, 0,
-                   2, 5, 1, 3, 0, 4,
-                   1, 3, 2, 5, 4, 0,
-                   4, 1, 5, 0, 2, 3,
-                   3, 1, 5, 2, 4, 0,
-                   5, 4, 1, 3, 2, 0]
-        s_prefs = reshape(s_prefs, n+1, m)
+    # Students' preference orders over colleges 1, ..., 5 and unmatched 0
+    s_prefs = [3, 1, 5, 4, 0, 2,
+               1, 3, 4, 2, 5, 0,
+               4, 5, 3, 1, 2, 0,
+               3, 4, 1, 5, 0, 2,
+               1, 4, 2, 0, 3, 5,
+               4, 3, 2, 1, 5, 0,
+               2, 5, 1, 3, 0, 4,
+               1, 3, 2, 5, 4, 0,
+               4, 1, 5, 0, 2, 3,
+               3, 1, 5, 2, 4, 0,
+               5, 4, 1, 3, 2, 0]
+    s_prefs = reshape(s_prefs, n+1, m)
 
-        # Colleges' preference orders over students 1, ..., 11 and unmatched 0
-        c_prefs = [3, 7, 9, 11, 5, 4, 10, 8, 6, 1,
-                   2, 0, 5, 7, 10, 6, 8, 2, 3, 11,
-                   0, 1, 4, 9, 11, 6, 8, 3, 2, 4,
-                   7, 1, 10, 0, 5, 9, 10, 1, 2, 11,
-                   4, 9, 5, 3, 6, 8, 0, 7, 2, 4,
-                   10, 7, 6, 1, 8, 3, 11, 9, 0, 5]
-        c_prefs = reshape(c_prefs, m+1, n)
+    # Colleges' preference orders over students 1, ..., 11 and unmatched 0
+    c_prefs = [3, 7, 9, 11, 5, 4, 10, 8, 6, 1,
+               2, 0, 5, 7, 10, 6, 8, 2, 3, 11,
+               0, 1, 4, 9, 11, 6, 8, 3, 2, 4,
+               7, 1, 10, 0, 5, 9, 10, 1, 2, 11,
+               4, 9, 5, 3, 6, 8, 0, 7, 2, 4,
+               10, 7, 6, 1, 8, 3, 11, 9, 0, 5]
+    c_prefs = reshape(c_prefs, m+1, n)
 
-        # Capacities for colleges
-        caps = [4, 1, 3, 2, 1]
-        indptr_expected = [1, 5, 6, 9, 11, 12]
+    # Capacities for colleges
+    caps = [4, 1, 3, 2, 1]
+    indptr = [1, 5, 6, 9, 11, 12]
 
-        # Unique stable matching
-        s_matched_expected = [3, 1, 4, 3, 1, 3, 2, 1, 4, 1, 5]
-        c_matched_expected = [2, 5, 8, 10, 7, 1, 4, 6, 3, 9, 11]
+    # Optimal stable matchings
 
-        # Male proposal
-        s_matched_computed, c_matched_computed, indptr_computed =
-            fn(s_prefs, c_prefs, caps)
-        @test s_matched_computed == s_matched_expected
-        @test indptr_computed == indptr_expected
+    # Student optimal
+    s_matches_s_opt = [3, 1, 4, 3, 1, 3, 2, 1, 4, 1, 5]
+    c_matches_s_opt = [2, 5, 8, 10, 7, 1, 4, 6, 3, 9, 11]
 
-        # Sort matched students for each college
-        for j in 1:n
-            sort!(sub(c_matched_computed,
-                      indptr_expected[j]:indptr_expected[j+1]-1)
-            )
+    # Matching the book claims to be hospital-(or college-)optimal
+    # s_matches_c_opt = [4, 4, 3, 1, 1, 3, 2, 3, 1, 5, 1]
+    # c_matches_c_opt = [4, 5, 9, 11, 7, 3, 6, 8, 1, 2, 10]
+
+    # Matching actually hospital-(or college-)optimal
+    s_matches_c_opt = [4, 5, 3, 1, 1, 3, 2, 3, 1, 4, 1]
+    c_matches_c_opt = [4, 5, 9, 11, 7, 3, 6, 8, 1, 10, 2]
+
+    d = Dict(
+        "s_prefs" => s_prefs,
+        "c_prefs" => c_prefs,
+        "caps" => caps,
+        "s_matches_s_opt" => s_matches_s_opt,
+        "c_matches_s_opt" => c_matches_s_opt,
+        "s_matches_c_opt" => s_matches_c_opt,
+        "c_matches_c_opt" => c_matches_c_opt,
+        "indptr" => indptr,
+    )
+    push!(matchings_many_to_one, d)
+
+    # Test case: many-to-one; from Roth and Sotomayor Page 16
+    m, n = 7, 5
+    s_prefs = [
+        5, 1, 0, 2, 3, 4,
+        2, 5, 1, 0, 3, 4,
+        3, 1, 0, 2, 4, 5,
+        4, 1, 0, 2, 3, 5,
+        1, 2, 0, 3, 4, 5,
+        1, 3, 0, 2, 4, 5,
+        1, 3, 4, 0, 2, 6,
+    ]
+    s_prefs = reshape(s_prefs, n+1, m)
+    c_prefs = [
+        1, 2, 3, 4, 5, 6, 7, 0,
+        5, 2, 0, 1, 3, 4, 6, 7,
+        6, 7, 3, 0, 1, 2, 4, 5,
+        7, 4, 0, 1, 2, 3, 5, 6,
+        2, 1, 0, 3, 4, 5, 6, 7,
+    ]
+    c_prefs = reshape(c_prefs, m+1, n)
+    caps = [3, 1, 1, 1, 1]
+    indptr = [1, 4, 5, 6, 7,  8]
+    s_matches_s_opt = [5, 2, 3, 4, 1, 1, 1]
+    c_matches_s_opt = [5, 6, 7, 2, 3, 4, 1]
+    s_matches_c_opt = [1, 5, 1, 1, 2, 3, 4]
+    c_matches_c_opt = [1, 3, 4, 5, 6, 7, 2]
+    d = Dict(
+        "s_prefs" => s_prefs,
+        "c_prefs" => c_prefs,
+        "caps" => caps,
+        "s_matches_s_opt" => s_matches_s_opt,
+        "c_matches_s_opt" => c_matches_s_opt,
+        "s_matches_c_opt" => s_matches_c_opt,
+        "c_matches_c_opt" => c_matches_c_opt,
+        "indptr" => indptr,
+    )
+    push!(matchings_many_to_one, d)
+
+    @testset "one-to-one: Vector of Vectors" begin
+        for d in matchings_one_to_one
+            # Convert Matrix to Vector{Vector}
+            m_prefs, f_prefs = mat2vecs.([d["m_prefs"], d["f_prefs"]])
+
+            # Male proposal
+            m_matches, f_matches = _deferred_acceptance(m_prefs, f_prefs)
+            @test m_matches == d["m_matches_m_opt"]
+            @test f_matches == d["f_matches_m_opt"]
+
+            # Female proposal
+            f_matches, m_matches = _deferred_acceptance(f_prefs, m_prefs)
+            @test m_matches == d["m_matches_f_opt"]
+            @test f_matches == d["f_matches_f_opt"]
         end
-        @test c_matched_computed == c_matched_expected
     end
 
+    @testset "many-to-one: Vector of Vectors" begin
+        for d in matchings_many_to_one
+            s_prefs, c_prefs = mat2vecs.(d["s_prefs"], d["c_prefs"])
+
+            # Default (Student proposal)
+            s_matches, c_matches, indptr =
+                deferred_acceptance(s_prefs, c_prefs, d["caps"])
+            sort_matches!(c_matches, indptr)
+            @test s_matches == d["s_matches_s_opt"]
+            @test c_matches == d["c_matches_s_opt"]
+            @test indptr == d["indptr"]
+        end
+    end
+
+    if test_matrix
+        @testset "one-to-one: Matrix" begin
+            for d in matchings_one_to_one
+                m_prefs, f_prefs = d["m_prefs"], d["f_prefs"]
+
+                # Male proposal
+                m_matches, f_matches = _deferred_acceptance(m_prefs, f_prefs)
+                @test m_matches == d["m_matches_m_opt"]
+                @test f_matches == d["f_matches_m_opt"]
+
+                # Female proposal
+                f_matches, m_matches = _deferred_acceptance(f_prefs, m_prefs)
+                @test m_matches == d["m_matches_f_opt"]
+                @test f_matches == d["f_matches_f_opt"]
+            end
+        end
+
+        @testset "many-to-one: Matrix" begin
+            for d in matchings_many_to_one
+                s_prefs, c_prefs = d["s_prefs"], d["c_prefs"]
+
+                # Default (Student proposal)
+                s_matches, c_matches, indptr =
+                    deferred_acceptance(s_prefs, c_prefs, d["caps"])
+                sort_matches!(c_matches, indptr)
+                @test s_matches == d["s_matches_s_opt"]
+                @test c_matches == d["c_matches_s_opt"]
+                @test indptr == d["indptr"]
+            end
+        end
+    end
 end
