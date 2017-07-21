3ee5e96d24fe76b332dcbfad28e8d246327c3e93
佐々木彬弘 <sasakiakihiro@sasakiakirahiroshi-no-MacBook-Air.local>
2017-07-21 10:56:51 +0900

    a

--- src/MyMatchingA.jl
+++ src/MyMatchingA.jl
@@ -89,7 +89,7 @@ function my_deferred_acceptance(prop_prefs::Matrix{Int}, resp_prefs::Matrix{Int}
       m = size(prop_prefs, 1)
       n = size(resp_prefs, 1)
     
-      M = sum(caps1)
+@show      M = sum(caps1)
       prop_matches = fill(-1, M)
 
       prop_next_to_propose = ones(Int64, m)
